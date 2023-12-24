39c360d49c9e9fa67712b89299209136d6db806f
Bogdan Pechounov <bogdan.pechounov@outlook.com>
2023-12-24 17:44:55 -0500

    dropdown menu

--- compose-preferences/build.gradle.kts
+++ compose-preferences/build.gradle.kts
@@ -36,7 +36,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.3"
+        kotlinCompilerExtensionVersion = "1.5.3"
     }
 }
 
@@ -44,13 +44,13 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
+    implementation("com.google.android.material:material:1.11.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
     // Compose
-    implementation("androidx.activity:activity-compose:1.8.1")
+    implementation("androidx.activity:activity-compose:1.8.2")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
---
+++ compose-preferences/src/main/java/com/uzential/compose_preferences/ui/preferences/DropdownPreference.kt
@@ -0,0 +1,180 @@
+package com.uzential.compose_preferences.ui.preferences
+
+import android.widget.Toast
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.filled.ThumbUp
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.uzential.compose_preferences.data.Preference
+import com.uzential.compose_preferences.data.compose.state
+import com.uzential.compose_preferences.ui.PreferenceItem
+import com.uzential.compose_preferences.ui.components.Choice
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun <V> DropdownPreference(
+    title: String,
+    choices: List<Choice<out V?>>,
+    preference: Preference<V>,
+    description: @Composable (() -> Unit)? = null,
+    icon: @Composable (() -> Unit)? = null,
+){
+    var state by preference.state()
+
+    DropdownPreference(
+        title = { Text(text = title) },
+        choices = choices,
+        selected = state,
+        onSelect = { state = it },
+        description = description,
+        icon = icon
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun <V> DropdownPreference(
+    title: @Composable () -> Unit,
+    choices: List<Choice<out V>>,
+    selected: V,
+    onSelect: (V) -> Unit,
+    modifier: Modifier = Modifier,
+    description: @Composable (() -> Unit)? = null,
+    icon: @Composable (() -> Unit)? = null,
+) {
+    var expanded by remember {
+        mutableStateOf(false)
+    }
+
+    val selectedChoice = choices.find { it.value == selected } ?: return
+
+    PreferenceItem(
+        modifier = modifier,
+        title = title,
+        description = description,
+        icon = icon,
+        onClick = {
+            expanded = !expanded
+        },
+        action = {
+            Box {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                ) {
+                    Text(
+                        text = selectedChoice.displayValue,
+                        modifier = Modifier.padding(4.dp)
+                    )
+                    ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded)
+                }
+
+
+                DropdownMenu(
+                    expanded = expanded,
+                    onDismissRequest = { expanded = false }
+                ) {
+                    choices.forEach { choice ->
+                        DropdownMenuItem(
+                            text = {
+                                Text(
+                                    modifier = Modifier.weight(1f),
+                                    text = choice.displayValue
+                                )
+                            },
+                            onClick = {
+                                onSelect(choice.value)
+                                expanded = false
+                            }
+                        )
+                    }
+                }
+            }
+        }
+    )
+}
+
+
+@Composable
+fun Demo_DropDownMenu() {
+    val context = LocalContext.current
+    var expanded by remember { mutableStateOf(false) }
+
+    Box(
+        modifier = Modifier
+            //.fillMaxWidth()
+            .wrapContentSize(Alignment.TopEnd)
+    ) {
+        IconButton(onClick = { expanded = !expanded }) {
+            Icon(
+                imageVector = Icons.Default.MoreVert,
+                contentDescription = "More"
+            )
+        }
+
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false }
+        ) {
+            DropdownMenuItem(
+                text = { Text("L") },
+                onClick = { Toast.makeText(context, "L", Toast.LENGTH_SHORT).show() }
+            )
+            DropdownMenuItem(
+                text = { Text("Save a very super long word") },
+                onClick = {
+                    Toast.makeText(
+                        context,
+                        "Save a very super long word",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun DropdownPreferencePreview() = Surface {
+    val choices = listOf(
+        Choice(false, "L"),
+        Choice(true, "Dark"),
+        Choice(null, "Device")
+    )
+
+    var selected: Boolean? by remember {
+        mutableStateOf(null)
+    }
+
+    DropdownPreference(
+        title = { Text(text = "Theme") },
+        icon = {
+            Icon(imageVector = Icons.Default.ThumbUp, contentDescription = null)
+        },
+        choices = choices,
+        selected = selected,
+        onSelect = { selected = it }
+    )
+}
